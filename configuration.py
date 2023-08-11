4160d04ec44e94ac85a4e2e24e7d3a7d1fcda172
MayBe <glummagda@yandex.ru>
2023-08-11 21:35:12 +0300

    First one

---
+++ configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE = "https://936f4f67-9188-41bb-827b-4f5955cd6139.serverhub.praktikum-services.ru"
+DOC_PATH = "/api/db/resources/{table_name}.csv"
+USERS_TABLE_PATH = '/api/db/resources/user_model.csv'
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
